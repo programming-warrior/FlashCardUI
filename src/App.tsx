a31664fc9d36bd1cc9c9b29a9824739114ff12f6
Yusuf <mdyusuf2521@gmail.com>
2024-08-11 14:06:43 +0530

    Added text gradient and button svg

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import { useState } from 'react'
 import './App.css'
 import FlashCard from './Components/FlashCard/FlashCard'
+import RightArrow from './Components/RightArrow';
+import LeftArrow from "./Components/LeftArrow";
 
 function App() {
 
@@ -39,14 +41,14 @@ function App() {
 
   return (
     <div className='min-h-screen flex items-center justify-between px-8 overflow-hidden'>
-      <button className='mr-4' onClick={handlePrev}>
-        prev
+      <button className='mr-4 w-10 h-10' onClick={handlePrev}>
+        <LeftArrow/>
       </button>
 
       <div className="w-full overflow-hidden border border-black">
         <div
-          className="flex transition-transform"
-          style={{ transform: `translateX(-${current * 100}%)` }}
+          className="flex "
+          style={{ transform: `translateX(-${current * 100}%)` ,transition:"all 0.5s"}}
         >
           {data.map((d,) => (
             <div className="min-w-full flex justify-center transition-transform" key={d.id}>
@@ -56,8 +58,8 @@ function App() {
         </div>
       </div>
 
-      <button className='ml-4' onClick={handleNext}>
-        next
+      <button className='ml-4 w-10 h-10 ' onClick={handleNext}>
+        <RightArrow/>
       </button>
     </div>
   )
