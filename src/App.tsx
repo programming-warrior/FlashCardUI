0b886ef9ffc429426f86ad33c806130dd82cc7cf
Yusuf <mdyusuf2521@gmail.com>
2024-08-11 18:21:58 +0530

    integrated the fetch api

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,29 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 import FlashCard from './Components/FlashCard/FlashCard'
 import RightArrow from './Components/RightArrow';
 import LeftArrow from "./Components/LeftArrow";
+import axios from 'axios';
 
 function App() {
 
-  const data = [
-    {
-      id: 1,
-      question: "Question 1",
-      answer: "Answer 1",
-    },
-    {
-      id: 2,
-      question: "Question 2",
-      answer: "Answer 2",
-    },
-    {
-      id: 3,
-      question: "Question 3",
-      answer: "Answer 3",
-    },
-    {
-      id: 4,
-      question: "Question 4",
-      answer: "Answer 4",
-    }
-  ];
+  const BACKEND_URL=import.meta.env.VITE_BACKEND_URL;
+  const [data,setData]=useState([]);
 
   const [current, setCurrent] = useState(0);
+
+  useEffect(()=>{
+    async function fetchData(){
+      const res=await axios.get(`${BACKEND_URL}/api/fetch`);
+      if(res.status==200 || res.status==201){
+        setData(res.data);
+      }
+      return [];
+    }
+
+    fetchData();
+
+  },[])
 
   const handlePrev = () => {
     setCurrent((prev) => (prev === 0 ? prev : prev - 1));
@@ -40,25 +34,25 @@ function App() {
   }
 
   return (
-    <div className='min-h-screen flex items-center justify-between px-8 overflow-hidden'>
-      <button className='mr-4 w-10 h-10' onClick={handlePrev}>
+    <div className='min-h-screen flex items-center justify-between px-3 sm:px-8 overflow-hidden'>
+      <button className=' w-10 h-10' onClick={handlePrev}>
         <LeftArrow/>
       </button>
 
       <div className="w-full overflow-hidden border border-black">
         <div
-          className="flex "
+          className="flex"
           style={{ transform: `translateX(-${current * 100}%)` ,transition:"all 0.5s"}}
         >
           {data.map((d,) => (
-            <div className="min-w-full flex justify-center transition-transform" key={d.id}>
+            <div className="min-w-full px-2 flex justify-center transition-transform" key={d.id}>
               <FlashCard data={d} />
             </div>
           ))}
         </div>
       </div>
 
-      <button className='ml-4 w-10 h-10 ' onClick={handleNext}>
+      <button className=' w-10 h-10 ' onClick={handleNext}>
         <RightArrow/>
       </button>
     </div>
